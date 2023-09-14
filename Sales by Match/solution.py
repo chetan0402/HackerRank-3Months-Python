ad8c0250f0ecca828b9549dd39882afa1e103173
Chetan0402 <chetansharma67g@gmail.com>
2023-09-14 17:40:07 +0530

    pair matching

---
+++ Sales by Match/solution.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    tracker={}
+    to_return=0
+    for ele in ar:
+        try:
+            tracker[ele]+=1
+        except:
+            tracker[ele]=1
+
+    for key in tracker.keys():
+        div=math.floor(tracker[key]/2)
+        to_return+=div
+
+    return to_return
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
